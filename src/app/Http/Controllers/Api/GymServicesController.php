3341e0665684cf7c4fe8c11219a9320a108701cf
olexandr.genov <olexandr.genov@likebus.ua>
2026-05-21 19:31:26 +0300

    Enhance GymServicesController to include customer-specific sales calculations
    - Updated the index method to accept a Telegram ID and validate the request.
    - Implemented logic to calculate service prices based on customer status (military member or student) and applied relevant discounts.
    - Adjusted the API response to return a refined array of services with calculated sale prices, improving the overall service information provided to clients.

--- src/app/Http/Controllers/Api/GymServicesController.php
+++ src/app/Http/Controllers/Api/GymServicesController.php
@@ -4,17 +4,60 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\GymService;
+use Illuminate\Http\Request;
 
 class GymServicesController extends Controller
 {
-    public function index()
+    public function index( Request $request)
     {
+
+        $data = $request->validate([
+            'telegram_id' => ['nullable', 'integer'],
+        ]);
+
         $services = GymService::query()
             ->orderBy('id')
             ->get(['id', 'name', 'price', 'description', 'is_active', 'is_periodical', 'day_amount', 'visit_amount', 'sales_default', 'sales_military_member', 'sales_student']);
 
+
+        $customer = Customer::query()
+            ->where('telegram_id', (int) $data['telegram_id'])
+            ->first();
+        if (! $customer) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Customer not found',
+                'code' => 4,
+            ], 404);
+        }
+        $finalServicesArray = [];
+        foreach ($services as $service) {
+            $amount = (float) $service->price;
+            $currency = (string) config('services.wayforpay.currency', 'UAH');
+
+            $sale = $service->sales_default;
+            if ($customer->is_military_member) {
+                $sale = $service->sales_military_member;
+            }
+            if ($customer->is_student) {
+                $sale = $service->sales_student;
+            }
+
+
+            if ($sale > 0) {
+                $amount = ceil((float) $service->price / 100 * (100 - $sale));
+            }
+
+            $finalServicesArray[] = [
+                'id' => $service->id,
+                'name' => $service->name,
+                'price' => $service->price,
+                'sale' => $amount,
+                'description' => $service->description,
+            ];
+        }
         return response()->json([
-            'data' => $services,
+            'data' => $finalServicesArray,
         ]);
     }
 }
