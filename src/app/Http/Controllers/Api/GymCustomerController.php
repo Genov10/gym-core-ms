b97aeebb6bf857df605c53ace911361f9762e2f9
olexandr.genov <olexandr.genov@likebus.ua>
2026-09-17 07:14:01 +0300

    Fix typo in customer gym services name from 'Гостевий візит' to 'Гостьовий візит' in GymCustomerController.

--- src/app/Http/Controllers/Api/GymCustomerController.php
+++ src/app/Http/Controllers/Api/GymCustomerController.php
@@ -109,7 +109,7 @@ class GymCustomerController extends Controller
         if ($isGuestVisitAvailable == 1) {
             $services[] = [
                 'id' => 0,
-                'name' => 'Гостевий візит',
+                'name' => 'Гостьовий візит',
             ];
         }
 
